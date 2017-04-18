c94bc3475d25d664340758e33baa2f392d23924a
yumi <iyumi769@gmail.com>
2017-04-18 07:09:14 +0000

    add top page

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  before_save { self.email.downcase! } #保存前に小文字に変換
+  validates :name, presence: true, length: { maximum: 50 } #null NG, ~50 characters
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }, #正規表現指定
+                    uniqueness: { case_sensitive: false } #重複不可
+  has_secure_password
+end
+
